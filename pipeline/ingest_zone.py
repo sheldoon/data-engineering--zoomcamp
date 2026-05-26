499db758bcbe120d685fda5ff40d380cce57ee68
João Pedro <84822895+sheldoon@users.noreply.github.com>
2026-05-26 02:26:55 +0000

    update files

--- pipeline/ingest_zone.py
+++ pipeline/ingest_zone.py
@@ -7,28 +7,12 @@ from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
 dtype = {
-    "VendorID": "Int64",
-    "passenger_count": "Int64",
-    "trip_distance": "float64",
-    "RatecodeID": "Int64",
-    "store_and_fwd_flag": "string",
-    "PULocationID": "Int64",
-    "DOLocationID": "Int64",
-    "payment_type": "Int64",
-    "fare_amount": "float64",
-    "extra": "float64",
-    "mta_tax": "float64",
-    "tip_amount": "float64",
-    "tolls_amount": "float64",
-    "improvement_surcharge": "float64",
-    "total_amount": "float64",
-    "congestion_surcharge": "float64"
+    "LocationID": "Int64",
+    "Borough": "string",
+    "Zone": "string",
+    "service_zone": "string"
 }
 
-parse_dates = [
-    "tpep_pickup_datetime",
-    "tpep_dropoff_datetime"
-]
 
 
 @click.command()
@@ -40,7 +24,7 @@ parse_dates = [
 @click.option('--year', default=2021, type=int, help='Year for data')
 @click.option('--month', default=1, type=int, help='Month for data')
 @click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
-@click.option('--table_name', default='yellow_taxi_data', help='Table name')
+@click.option('--table_name', default='zone_taxi_data', help='Table name')
 def run(pg_user, pg_password, pg_host, pg_port, pg_database, year, month, chunksize, table_name):
     prefix = 'https://d37ci6vzurychx.cloudfront.net/misc/'
     url = prefix + 'taxi_zone_lookup.csv'
@@ -49,7 +33,6 @@ def run(pg_user, pg_password, pg_host, pg_port, pg_database, year, month, chunks
     df_iter = pd.read_csv(
         url,
         dtype=dtype,
-        parse_dates=parse_dates,
         iterator=True,
         chunksize=chunksize,
     )
